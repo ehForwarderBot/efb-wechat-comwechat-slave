310fa9bd6ea0b992076a58cad3e1dfa8701a9189
jiz4oh <41264693+jiz4oh@users.noreply.github.com>
2025-07-24 15:59:58 +0800

    feat: add group announcement (#53)
    * feat: add chatroom announcement message
    
    * feat: remove duplicated type 87 announcement
    
    * feat: at on group announcement
    
    * fix: ignore textannouncement properly

--- efb_wechat_comwechat_slave/MsgDeco.py
+++ efb_wechat_comwechat_slave/MsgDeco.py
@@ -539,12 +539,13 @@ def efb_share_link_wrapper(message: dict, chat) -> Message:
                 vendor_specific={ "is_mp": True }
             )
         elif type == 87: # 群公告
-            title = xml.xpath('/msg/appmsg/textannouncement/text()')[0]
-            efb_msg = Message(
-                type=MsgType.Text,
-                text= f"[群公告]:\n{title}" ,
-                vendor_specific={ "is_mp": False }
-            )
+            return
+        #     title = xml.xpath('/msg/appmsg/textannouncement/text()')[0]
+        #     efb_msg = Message(
+        #         type=MsgType.Text,
+        #         text= f"[群公告]:\n{title}" ,
+        #         vendor_specific={ "is_mp": False }
+        #     )
         elif type == 2000:
             subtype = xml.xpath("/msg/appmsg/wcpayinfo/paysubtype/text()")[0]
             money =  xml.xpath("/msg/appmsg/wcpayinfo/feedesc/text()")[0].strip("<![CDATA[").strip("]]>")
@@ -673,7 +674,7 @@ def efb_voice_wrapper(file: IO, filename: str = None, text: str = None) -> Messa
         efb_msg.text = text
     return efb_msg
 
-def efb_other_wrapper(text: str) -> Union[Message, None]:
+def efb_other_wrapper(text: str, chat) -> Union[Message, None]:
     """
     A simple EFB message wrapper for other message
     :param text: The content of the message
@@ -754,6 +755,17 @@ def efb_other_wrapper(text: str) -> Union[Message, None]:
             text= None,
             vendor_specific={ "is_mp": False }
             )
+
+    elif msg_type == "mmchatroombarannouncememt":
+        title = xml.xpath('/sysmsg/mmchatroombarannouncememt/content/text()')[0]
+        at_list = {}
+        at_list[(1, 4)] = chat.self
+        efb_msg = Message(
+            type=MsgType.Text,
+            text= f"[群公告]:\n{title}" ,
+            vendor_specific={ "is_mp": False },
+            substitutions = Substitutions(at_list)
+        )
 
     if efb_msg:
         return efb_msg
